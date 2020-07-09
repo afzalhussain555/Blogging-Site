3b816e3ea0d4efb2448ccc69b2b47e93d4072046
Afzal Hussain <afzalhussain555@gmail.com>
2020-07-09 19:01:26 +0530

    New route created,Adding new Blogs

--- app.js
+++ app.js
@@ -40,7 +40,18 @@ app.get("/blogs",function(req,res){
     })
 })
 
+app.get("/blogs/new", function(req, res){
+    res.render("new")
+})
 
+app.post("/blogs",function(req,res){
+    Blog.create(req.body.blog,function(err,newblog){
+        if(err)
+        res.render("new")
+        else
+        res.redirect("/blogs");
+    })
+})
 
 app.listen(3000,function(){
     console.log("Server is running now")
